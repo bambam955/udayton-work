4d22b970f30daa6dbe32ce4b1260b8357c702d5d
Bennett Moore <bennettmoore05@gmail.com>
2025-09-09 14:54:39 -0400

    feat: add files for part 1 of homework 1

---
+++ cps-444/homework-1/greeting/greeting.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// Corresponds to part 1 problem 1
+func main() {
+	fmt.Println("Hello, World!")
+}
---
+++ cps-444/homework-1/name_box/name_box.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// Corresponds to part 1 problem 2
+func main() {
+	mhy := os.Args[1]
+	fmt.Println(mhy, os.Args[0])
+}
