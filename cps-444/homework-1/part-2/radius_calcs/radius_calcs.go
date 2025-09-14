52ea13531aa9dab4c42b118f5b1064d040067a65
Bennett Moore <bennettmoore05@gmail.com>
2025-09-13 21:59:00 -0400

    feat: implement circle and sphere calculations

--- cps-444/homework-1/part-2/radius_calcs/radius_calcs.go
+++ cps-444/homework-1/part-2/radius_calcs/radius_calcs.go
@@ -2,6 +2,42 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
+const helpMsg string = `USAGE: radius_calcs.go RADIUS`
+
+func main() {
+	// Verify args number.
+	if len(os.Args) < 2 {
+		fmt.Println(helpMsg)
+		return
+	}
+
+	radius, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing radius from %s.\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	PrintCircleInfo(radius)
+	PrintSphereInfo(radius)
+}
+
+func PrintCircleInfo(radius float64) {
+	area := math.Pi * math.Pow(radius, 2)
+	circumference := 2 * math.Pi * radius
+	fmt.Printf("Circle area\t= %.2f\n", area)
+	fmt.Printf("Circle circum.\t= %.2f\n", circumference)
+}
+
+func PrintSphereInfo(radius float64) {
+	sphereVol := (4 / 3) * math.Pi * math.Pow(radius, 3)
+	sphereSurfaceArea := 4 * math.Pi * math.Pow(radius, 2)
+	fmt.Printf("Sphere volume\t= %.2f\n", sphereVol)
+	fmt.Printf("Sphere SA\t= %.2f\n", sphereSurfaceArea)
 }
