1fe1ef6939eb2ebf73e597a1215581ccc85dfc71
Bennett Moore <bennettmoore05@gmail.com>
2025-09-26 19:45:02 -0400

    chore: add comments and module file

--- cps-444/homework-2/graph/graph.go
+++ cps-444/homework-2/graph/graph.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 
+// This constant determines the size of the graph, i.e. the number of vertices.
+// The graph is initialized with an adjacency matrix containing this many vertices,
+// identified by their index in the array.
 const size = 10
 
 type Graph struct {
 	matrix [size][size]bool
 }
 
-// AddEdge adds a directed edge from "from" to "to".
+// AddEdge adds a directed edge from "from" to "to". This is a DIGRAPH.
+// "from" and "to" must be values from 0-(size-1).
 func AddEdge(g *Graph, from, to int) bool {
 	if from >= size || to >= size {
 		fmt.Printf("AddEdge: error: graph only has %d vertices\n", size)
