eeed68dc698740ec361afd473aebdedd8277e749
Bennett Moore <120052232+bambam955@users.noreply.github.com>
2025-10-07 00:19:04 -0400

    feat(cps-444): homework 3 (#7)
    * feat: implement basics of argument parsing
    
    * feat: improve the helpmsg
    
    * feat: check for invalid options
    
    * feat: check for invalid files passed as arguments
    
    * feat: implement the stats tracking of all the files
    
    * feat: do not print the total if there is only one file
    
    * chore: remove build artifact
    
    * feat: handle input from stdin if no files passed
    
    * chore: a couple more comments and remove the TODO

---
+++ cps-444/homework-3/cmd.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/pflag"
+)
+
+type WcStatFlags struct {
+	showNewlines bool
+	showWords    bool
+	showBytes    bool
+}
+
+// Create the set of flags for our wc command.
+var fs = pflag.NewFlagSet("wc", pflag.ContinueOnError)
+var lines = fs.BoolP("lines", "l", false, "print the newline counts")
+var words = fs.BoolP("words", "w", false, "print the word counts")
+var chars = fs.BoolP("chars", "m", false, "print the character counts")
+var helpFlag = fs.Bool("help", false, "display this help and exit")
+
+func init() {
+	// Any flag-related error messages will be printed to stderr.
+	fs.SetOutput(os.Stderr)
+
+	fs.Usage = func() {
+		if f := fs.Lookup("help"); f != nil {
+			f.Hidden = true
+		}
+		var msg = `Usage: wc [OPTION]... [FILE]...
+  or:  wc [OPTION]... --files0-from=F
+Print newline, word, and byte counts for each FILE, and a total line if
+more than one FILE is specified.  A word is a non-zero-length sequence of
+printable characters delimited by white space.
+
+With no FILE, or when FILE is -, read standard input.
+
+The options below may be used to select which counts are printed, always in
+the following order: newline, word, character, byte, maximum line length.`
+		// The help message is printed to stdout.
+		fs.SetOutput(os.Stdout)
+		fmt.Println(msg)
+		fs.PrintDefaults()
+		fmt.Println("\n  --help     display this help and exit")
+	}
+}
+
+func Execute() error {
+	// If there is some sort of invalid option then print an error message and return early.
+	err := fs.Parse(os.Args[1:])
+	if err != nil {
+		return checkInvalidOptions(err)
+	}
+
+	// Verify that all of the arguments passed are valid filenames.
+	files := fs.Args()
+	// If no files were passed then we will read from stdin.
+	if len(files) == 0 {
+		files = []string{""}
+	} else if err := checkInvalidFiles(files); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return err
+	}
+
+	// Print the help message if the flag was passed.
+	if *helpFlag {
+		fs.Usage()
+		return nil
+	}
+
+	// If at least one of the flags was passed then just use that flag.
+	// However, if no flags were passed then all the flags are true.
+	var statFlags WcStatFlags
+	if *lines || *words || *chars {
+		statFlags.showNewlines = *lines
+		statFlags.showWords = *words
+		statFlags.showBytes = *chars
+	} else {
+		statFlags.showNewlines = true
+		statFlags.showWords = true
+		statFlags.showBytes = true
+	}
+
+	// Output all of the file stats.
+	return OutputFileStats(files, statFlags)
+}
+
+func checkInvalidOptions(err error) error {
+	var invalidOption string
+	// If the erroneous flag is a shorthand flag (i.e. a one-letter abbreviation)
+	// then the error will contain "shorthand flag". If the erroneous flag is a fully
+	// described flag then the error will contain "unknown flag". We can use these
+	// to get the error messages exactly right.
+	if strings.Contains(err.Error(), "shorthand flag") {
+		// An example error message might look something like this:
+		// "unknown shorthand flag: 'a' in -asef"
+		start := strings.Index(err.Error(), "'")
+		if start != -1 {
+			end := strings.Index(err.Error()[start+1:], "'")
+			if end != -1 {
+				invalidOption = err.Error()[start+1 : start+1+end]
+			}
+		}
+		// For some reason wc has different error messages for shorthand and full-length flags.
+		// Here we print "invalid option", down farther we use "unrecognized option".
+		if invalidOption != "" {
+			fmt.Fprintf(os.Stderr, "wc: invalid option -- '%s'\n", invalidOption)
+		}
+	} else if strings.Contains(err.Error(), "unknown flag") {
+		// An example error message might look like this:
+		// "unknown flag: --wawmlsefasef", which is much easier to parse.
+		parts := strings.Fields(err.Error())
+		if len(parts) > 0 {
+			flagPart := parts[len(parts)-1]
+			if strings.HasPrefix(flagPart, "--") {
+				invalidOption = flagPart
+			}
+		}
+		// Here is where we use "unrecognized option" as the error message to match the real command.
+		if invalidOption != "" {
+			fmt.Fprintf(os.Stderr, "wc: unrecognized option '%s'\n", invalidOption)
+		}
+	}
+	// If we weren't able to figure out what the invalid option then print the whole error.
+	// Hopefully we can modify the program to parse it correctly.
+	if invalidOption == "" {
+		fmt.Fprintf(os.Stderr, "wc: %v\n", err)
+	}
+	// End with a help message.
+	fmt.Fprintf(os.Stderr, "Try 'wc --help' for more information.\n")
+	return fmt.Errorf("invalid option")
+}
+
+func checkInvalidFiles(files []string) error {
+	for _, file := range files {
+		// This is what is optionally passed to use stdin, so we can skip it.
+		if file == "-" {
+			continue
+		}
+		info, err := os.Stat(file)
+		// First, check if the file exists. If not then return that.
+		if os.IsNotExist(err) {
+			return fmt.Errorf("wc: %s: No such file or directory", file)
+		}
+		// If there is some other weird error then return that.
+		if err != nil {
+			return fmt.Errorf("wc: %s: %v", file, err)
+		}
+		// Finally, if there is no error, but the "file" is a directory, that's not allowed.
+		// Return an error explaining the problem.
+		if info.IsDir() {
+			return fmt.Errorf("wc: %s: Is a directory", file)
+		}
+	}
+	return nil
+}
---
+++ cps-444/homework-3/wcstats.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"unicode"
+)
+
+// This is the main function.
+func OutputFileStats(files []string, flags WcStatFlags) error {
+	// If the first and only argument is an empty string then we should read from stdin.
+	readingStdin := files[0] == "" && len(files) == 1
+	// This slice will contain all of the stats, so that they can be outputted at the end.
+	var allStats []wcStats
+	// These will be the aggregate stats.
+	var totalStats wcStats
+
+	// Iterate through each of the files and count up all the newlines, words, and bytes in each one.
+	for _, file := range files {
+		var contents []byte
+		var readErr error
+		// Read text from the correct source.
+		if readingStdin {
+			contents, readErr = io.ReadAll(os.Stdin)
+		} else {
+			contents, readErr = os.ReadFile(file)
+		}
+		if readErr != nil {
+			return readErr
+		}
+
+		// Count the stats and append to the slice.
+		stats := countStatsForText(string(contents))
+		allStats = append(allStats, stats)
+		// Add to the aggregate total.
+		totalStats.add(stats)
+		// Update the table cell size if necessary.
+		// Note that we can't output anything until we go through all of the files
+		// because we don't know in advance what the table cell size will be.
+		if readingStdin {
+			// 7 is the standard size for the table when reading from stdin.
+			// Not sure why, but that's what it is.
+			tableCellFmtLen = max(7, stats.determineFmtSize(flags))
+		} else {
+			tableCellFmtLen = max(tableCellFmtLen, stats.determineFmtSize(flags))
+		}
+	}
+
+	// Print out the stats for each file.
+	for i, stats := range allStats {
+		fmt.Printf("%s %s\n", stats.toString(flags), files[i])
+	}
+
+	// Print out the total stats if there was more than one file.
+	// If there is only one file then printing "totals" would be redundant.
+	if len(allStats) > 1 {
+		fmt.Printf("%s total\n", totalStats.toString(flags))
+	}
+	return nil
+}
+
+// Track all of the stats for a file.
+type wcStats struct {
+	newlines int
+	words    int
+	bytes    int
+}
+
+// This filewide global is used to have a consistent "cell size" for each value
+// in the table produced when the stats are displayed.
+var tableCellFmtLen int
+
+// This function converts a file's stats to a string according to
+// the command options and cell size.
+func (w *wcStats) toString(flags WcStatFlags) string {
+	outputStr := ""
+	if flags.showNewlines {
+		outputStr += fmt.Sprintf("%*d ", tableCellFmtLen, w.newlines)
+	}
+	if flags.showWords {
+		outputStr += fmt.Sprintf("%*d ", tableCellFmtLen, w.words)
+	}
+	if flags.showBytes {
+		outputStr += fmt.Sprintf("%*d ", tableCellFmtLen, w.bytes)
+	}
+
+	// Trim off any extra whitespace on the end.
+	return strings.TrimRightFunc(outputStr, unicode.IsSpace)
+}
+
+// Figure out the string length of each value in the stats for a file and determine
+// if any of the stats are large enough as a string to increase the table cell size.
+func (w *wcStats) determineFmtSize(flags WcStatFlags) int {
+	newlinesFmtLen := len(fmt.Sprintf("%d", w.newlines))
+	wordsFmtLen := len(fmt.Sprintf("%d", w.words))
+	bytesFmtLen := len(fmt.Sprintf("%d", w.bytes))
+
+	maxCharLen := 3
+	if flags.showNewlines {
+		maxCharLen = max(maxCharLen, newlinesFmtLen)
+	}
+	if flags.showWords {
+		maxCharLen = max(maxCharLen, wordsFmtLen)
+	}
+	if flags.showBytes {
+		maxCharLen = max(maxCharLen, bytesFmtLen)
+	}
+
+	return maxCharLen
+}
+
+// Add the stats from one file to the stats from this one.
+func (w *wcStats) add(other wcStats) {
+	w.newlines += other.newlines
+	w.words += other.words
+	w.bytes += other.bytes
+}
+
+// Count up all of the bytes, words, and newlines from an input string.
+func countStatsForText(input string) wcStats {
+	var stats wcStats
+	stats.newlines = strings.Count(input, "\n")
+	stats.bytes = len(input)
+	stats.words = len(strings.Fields(input))
+	return stats
+}
