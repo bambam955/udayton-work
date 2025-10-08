74eb4d12c88890555b64e11cd3aeb89e39af62e9
Bennett Moore <bennettmoore05@gmail.com>
2025-10-07 21:46:46 -0400

    fix: make the activity timeouts work properly using socket timeouts

--- cps-470/project-1/TCP_server.py
+++ cps-470/project-1/TCP_server.py
@@ -16,36 +16,36 @@ def main():
     
     # Start the background process that periodically cleans out stale connection IDs.
     threading.Thread(target=cleanup_conn_ids, daemon=True).start()
-    
-    # Initialize the activity timer. If there are no connections
-    # for five minutes then server exits.
-    conn_timer: threading.Timer = None
-    restart_server_conn_timer(conn_timer)
 
     # Initialize the server socket.
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(("", args.server_port))
     server_socket.listen(1)
+    # If there is no activity for 5 minutes then the server exits.
+    server_socket.settimeout(5 * 60)
 
     # Begin waiting for connections.
     while True:
-        connection_socket, client_addr = server_socket.accept()
-        client_req = connection_socket.recv(1024).decode()
-        client_conn_id = int(client_req.split()[1])
+        try:
+            connection_socket, client_addr = server_socket.accept()
+            client_req = connection_socket.recv(1024).decode()
+            client_conn_id = int(client_req.split()[1])
 
-        restart_server_conn_timer(conn_timer)
+            res = ""
+            # Check to see if the client's connection ID was used already.
+            # If not, return "OK". If so, return "RESET".
+            if client_conn_id not in connection_ids:
+                connection_ids[client_conn_id] = time.time()
+                res = f"OK {client_conn_id} {client_addr[0]} {client_addr[1]}"
+            else:
+                res = f"RESET {client_conn_id}"
 
-        res = ""
-        # Check to see if the client's connection ID was used already.
-        # If not, return "OK". If so, return "RESET".
-        if client_conn_id not in connection_ids:
-            connection_ids[client_conn_id] = time.time()
-            res = f"OK {client_conn_id} {client_addr[0]} {client_addr[1]}"
-        else:
-            res = f"RESET {client_conn_id}"
+            connection_socket.send(res.encode())
+            connection_socket.close()
 
-        connection_socket.send(res.encode())
-        connection_socket.close()
+        # Exit the program if there is no activity.
+        except socket.timeout:
+            sys.exit(1)
 
 
 def cleanup_conn_ids():
@@ -56,13 +56,6 @@ def cleanup_conn_ids():
         to_remove = [k for k, v in connection_ids.items() if current - v >= 60]
         for k in to_remove:
             del connection_ids[k]
-
-
-def restart_server_conn_timer(conn_timer: threading.Timer):
-    if conn_timer:
-        conn_timer.cancel()
-    conn_timer = threading.Timer(5 * 60, sys.exit, args=(1,))
-    conn_timer.start()
 
 
 if __name__ == "__main__":
--- cps-470/project-1/UDP_server.py
+++ cps-470/project-1/UDP_server.py
@@ -16,34 +16,34 @@ def main():
     
     # Start the background process that periodically cleans out stale connection IDs.
     threading.Thread(target=cleanup_conn_ids, daemon=True).start()
-    
-    # Initialize the activity timer. If there are no connections
-    # for five minutes then server exits.
-    conn_timer: threading.Timer = None
-    restart_server_conn_timer(conn_timer)
 
     # Initialize the server socket.
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind(("", args.server_port))
+    # If there is no activity for 5 minutes then the server exits.
+    server_socket.settimeout(5 * 60)
 
     # Begin waiting for connections.
     while True:
-        client_raw_req, client_addr = server_socket.recvfrom(2048)
-        client_req = client_raw_req.decode()
-        client_conn_id = int(client_req.split()[1])
+        try:
+            client_raw_req, client_addr = server_socket.recvfrom(2048)
+            client_req = client_raw_req.decode()
+            client_conn_id = int(client_req.split()[1])
 
-        restart_server_conn_timer(conn_timer)
+            res = ""
+            # Check to see if the client's connection ID was used already.
+            # If not, return "OK". If so, return "RESET".
+            if client_conn_id not in connection_ids:
+                connection_ids[client_conn_id] = time.time()
+                res = f"OK {client_conn_id} {client_addr[0]} {client_addr[1]}"
+            else:
+                res = f"RESET {client_conn_id}"
 
-        res = ""
-        # Check to see if the client's connection ID was used already.
-        # If not, return "OK". If so, return "RESET".
-        if client_conn_id not in connection_ids:
-            connection_ids[client_conn_id] = time.time()
-            res = f"OK {client_conn_id} {client_addr[0]} {client_addr[1]}"
-        else:
-            res = f"RESET {client_conn_id}"
-
-        server_socket.sendto(res.encode(), client_addr)
+            server_socket.sendto(res.encode(), client_addr)
+        
+        # Exit the program if there is no activity.
+        except socket.timeout:
+            sys.exit(1)
 
 
 def cleanup_conn_ids():
@@ -54,13 +54,6 @@ def cleanup_conn_ids():
         to_remove = [k for k, v in connection_ids.items() if current - v >= 60]
         for k in to_remove:
             del connection_ids[k]
-
-
-def restart_server_conn_timer(conn_timer: threading.Timer):
-    if conn_timer:
-        conn_timer.cancel()
-    conn_timer = threading.Timer(5 * 60, sys.exit, args=(1,))
-    conn_timer.start()
 
 
 if __name__ == "__main__":
