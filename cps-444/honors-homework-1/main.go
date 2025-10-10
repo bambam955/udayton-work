5397e3079ea58300b2581d3902e475f83edf65f1
Bennett Moore <bennettmoore05@gmail.com>
2025-10-09 20:55:07 -0400

    docs: some more comments

--- cps-444/honors-homework-1/main.go
+++ cps-444/honors-homework-1/main.go
@@ -5,26 +5,31 @@ import (
 	"os"
 )
 
-// This program tests the implementation of the digraph.
+// This program tests the various pieces of the digraph implementation,
+// including construction, pathfinding, and cycle detection.
 
 func main() {
+	// We should have been passed a file name as the only argument. Verify this is the case.
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s GRAPH_FILE", os.Args[0])
 		os.Exit(1)
 	}
 
+	// Create the graph from the file. Exit immediately if there is an error.
 	graph, err := CreateGraphFromFile(os.Args[1])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(1)
 	}
 
+	// Output the created graph structure.
 	graph.DumpGraph()
 
+	// Ask the user for input on what node to trace distances from.
 	var inputNode string
 	fmt.Printf("start node? ")
 	fmt.Scanf("%s", &inputNode)
-
+	// Calculate the shortest distance from the inputted node to all other nodes in the graph.
 	for _, n := range graph.Nodes() {
 		dist, err := graph.MinDistance(inputNode, n.Id)
 		if err != nil {
@@ -34,9 +39,10 @@ func main() {
 		fmt.Printf("shortest distance from %s to %s: %d\n", inputNode, n.Id, dist)
 	}
 
+	// Check to see whether the graph has a cycle.
 	if graph.HasCycle() {
-		fmt.Println("The graph has a cycle")
+		fmt.Println("The graph has a cycle.")
 	} else {
-		fmt.Println("The graph does not have a cycle")
+		fmt.Println("The graph does not have a cycle.")
 	}
 }
