3b7f14b4dbbfa100d182b6f88b0957b68439f90b
Bennett Moore <bennettmoore05@gmail.com>
2025-11-06 17:57:13 -0500

    test: try big numbers for perfection

--- cps-444/homework-5/tests.c
+++ cps-444/homework-5/tests.c
@@ -74,4 +74,14 @@ void testPerfects()
             printFactors(i);
         }
     }
+
+    // Test some really big numbers.
+    for (int i = 1000; i <= 1000000000; i = i * 2 + 1234)
+    {
+        printf("Testing big number %d...\n", i);
+        if (isPerfect(i))
+        {
+            printFactors(i);
+        }
+    }
 }
