bee1294cd36d5f1fe36bf2acaf45e22447d09d85
Bennett Moore <bennettmoore05@gmail.com>
2025-11-06 00:55:40 -0500

    fix: slight debugging improvements

--- cps-470/project-2/team3proxy2.py
+++ cps-470/project-2/team3proxy2.py
@@ -37,6 +37,7 @@ while True:
         url_to_parse = 'http:/' + requested_url
     else:
         url_to_parse = requested_url
+    print('Requested URL:', url_to_parse)
     parsed_url = urlparse(url_to_parse)
     raw_filename = (parsed_url.netloc + parsed_url.path)
     print('Requested file:', raw_filename)
@@ -131,7 +132,7 @@ while True:
             print('\033[1;32mRetrieved from web server!\033[0m')
         
         except Exception as e:
-            print(f"Illegal request: {e}")
+            print(f"\033[1;36mIllegal request: {e}\033[0m")
             cli_sock.sendall(("HTTP/1.1 404 Not Found\r\n\r\n").encode())  
     
     # For some reason there was an error while reading the cached file or sending it back.
